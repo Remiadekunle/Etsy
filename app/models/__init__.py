c7bae8636b16bc566ba0fcf16a21aa2416ea9475
Remiadekunle <remiadekunle21@gmail.com>
2023-01-24 00:24:11 -0800

    updated __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,8 @@
 from .db import db
 from .user import User
+from .product import Product
+from .product_image import ProductImage
+from .order import Order
+from .cart import Cart
+from .cart_items import CartItem
 from .db import environment, SCHEMA
