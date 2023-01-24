8ac7dcca62075edf8c214dbb379080c88cf7d3a9
Remiadekunle <remiadekunle21@gmail.com>
2023-01-24 11:10:03 -0800

    created delete product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -55,3 +55,14 @@ def update_product(id):
         return {"product": product.to_dict()} , 201
     else:
         return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+@product_routes.route('/<int:id>', methods=['DELETE'])
+# @login_required
+def delete_product(id):
+    product = Product.query.get(id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return { "message": "Deleted"}, 200
+    else:
+        return {"message":'Product couldn\'t be found'} , 404
