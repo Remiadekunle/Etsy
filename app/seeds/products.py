b80b60f748141b5517fe4d2fc18bd0ee94d1ca2d
Remiadekunle <remiadekunle21@gmail.com>
2023-01-28 20:18:56 -0800

    changed the names to be in line with validation length

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -56,39 +56,39 @@ def seed_products():
         owner=owner3
     ),
     Product(
-        name='Call Of Duty Zombies Perks Bottle Replica', description='Perk bottle replica from call of duty black ops 3', price=200, stock=20, options='Juggernog-Speed Cola-Quick Revive-Stamin Up-Doulbe Tap-PHD Flopper- Widow\'s Wine', preview_img='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/09/Black-Ops-Perks.jpg',
+        name='COD Zombies Perks Bottle Replica', description='Perk bottle replica from call of duty black ops 3', price=200, stock=20, options='Juggernog-Speed Cola-Quick Revive-Stamin Up-Doulbe Tap-PHD Flopper- Widow\'s Wine', preview_img='https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/09/Black-Ops-Perks.jpg',
         owner=owner4
     ),
     Product(
-        name='Call Of Duty Zombies Gobble Gum Replica', description='Gobble gum replica from call of duty black ops 3', price=200, stock=20, options='Perkaholic-Anywhere but here-Raindrops-Unquenchable-Wall Power', preview_img='https://i.imgur.com/tPvMvEk.png',
+        name='COD Zombies Gobble Gum Replica', description='Gobble gum replica from call of duty black ops 3', price=200, stock=20, options='Perkaholic-Anywhere but here-Raindrops-Unquenchable-Wall Power', preview_img='https://i.imgur.com/tPvMvEk.png',
         owner=owner4
     ),
     Product(
-        name='Call Of Duty Zombies Replica Optic Gaming Skin Figure', description='Figure modeled from call of duty black ops 3 Optic Gaming skin', price=150, stock=20, options='Small-Medium-Large-XL-2XL', preview_img='https://www.charlieintel.com/wp-content/uploads/2015/08/Screen-Shot-2015-08-01-at-11.01.13-AM.png',
+        name='COD Replica Optic Gaming Skin Figure', description='Figure modeled from call of duty black ops 3 Optic Gaming skin', price=150, stock=20, options='Small-Medium-Large-XL-2XL', preview_img='https://www.charlieintel.com/wp-content/uploads/2015/08/Screen-Shot-2015-08-01-at-11.01.13-AM.png',
         owner=owner4
     ),
     Product(
-        name='Call Of Duty Zombies Replica Denial Skin Figure', description='Figure modeled from call of duty black ops 3 Denial Esports skin', price=150, stock=20, options='Small-Medium-Large-XL-2XL', preview_img='https://cdn.realsport101.com/images/ncavvykf/gfinityesports/44131efb6fed0a494795ddcf945f1b49dbed7ae0-744x433.png?rect=0,7,744,419&w=700&h=394&dpr=2',
+        name='COD Denial Skin Figure', description='Figure modeled from call of duty black ops 3 Denial Esports skin', price=150, stock=20, options='Small-Medium-Large-XL-2XL', preview_img='https://cdn.realsport101.com/images/ncavvykf/gfinityesports/44131efb6fed0a494795ddcf945f1b49dbed7ae0-744x433.png?rect=0,7,744,419&w=700&h=394&dpr=2',
         owner=owner4
     ),
     Product(
-        name='Call Of Duty Zombies Replica BAL-27 Skin', description='Gun modeled from call of duty advanced warfare gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/b7C_VovHQ4o/maxresdefault.jpg',
+        name='COD Replica BAL-27 Skin', description='Gun modeled from call of duty advanced warfare gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/b7C_VovHQ4o/maxresdefault.jpg',
         owner=owner5
     ),
     Product(
-        name='Call Of Duty Zombies Replica M8A1', description='Gun modeled from call of duty black ops 2 gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/CETdQb-Qz5E/maxresdefault.jpg',
+        name='COD Replica M8A1', description='Gun modeled from call of duty black ops 2 gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/CETdQb-Qz5E/maxresdefault.jpg',
         owner=owner5
     ),
     Product(
-        name='Call Of Duty Zombies Replica PDW-57', description='Gun modeled from call of duty black ops 2 gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/MZyR0IEXIQ4/maxresdefault.jpg',
+        name='COD Replica PDW-57', description='Gun modeled from call of duty black ops 2 gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/MZyR0IEXIQ4/maxresdefault.jpg',
         owner=owner5
     ),
     Product(
-        name='Call Of Duty Zombies Replica NV4', description='Gun modeled from call of duty infinite warfare gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/2iJfXNsTh5Q/maxresdefault.jpg',
+        name='COD Replica NV4', description='Gun modeled from call of duty infinite warfare gun', price=100, stock=20, options='Small-Large', preview_img='https://i.ytimg.com/vi/2iJfXNsTh5Q/maxresdefault.jpg',
         owner=owner5
     ),
     Product(
-        name='Call Of Duty Zombies Replica ACR', description='Gun modeled from call of duty modern warfare 3 gun', price=100, stock=20, options='Small-Large', preview_img='https://www.safarasoftair.com/media/catalog/product/cache/2/image/4e9d4c756f3a2e58dc27e2c20629df0a/m/a/masada3_tn/www.safarasoftair.com-Aandk-ak-masada3-ras-tn-31.jpg',
+        name='COD Replica ACR', description='Gun modeled from call of duty modern warfare 3 gun', price=100, stock=20, options='Small-Large', preview_img='https://www.safarasoftair.com/media/catalog/product/cache/2/image/4e9d4c756f3a2e58dc27e2c20629df0a/m/a/masada3_tn/www.safarasoftair.com-Aandk-ak-masada3-ras-tn-31.jpg',
         owner=owner5
     ),
     ]
