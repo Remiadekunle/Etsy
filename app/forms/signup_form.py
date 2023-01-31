11b570ed380fca131f4c08e13325f208db7a009e
Remiadekunle <remiadekunle21@gmail.com>
2023-01-31 15:11:20 -0800

    fixed error handling for products  and  orders

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -19,9 +19,6 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
-def valid_email(form, field):
-    email = field.data
-    
 
 
 class SignUpForm(FlaskForm):
