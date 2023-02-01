424dfc9d3731ace3c0992a59294783bb5c0bb66e
Remiadekunle <remiadekunle21@gmail.com>
2023-02-01 15:41:08 -0800

    added conditiona rendering for fav button based on if user exists

--- react-app/src/components/FavButton/index.js
+++ react-app/src/components/FavButton/index.js
@@ -5,10 +5,10 @@ import './index.css';
 function FavButton({productId}){
     const user = useSelector(state => state.session.user)
     const dispatch = useDispatch()
-    const favs = user.favIds
+    const favs = user?.favIds
     console.log('hey were actually getting the user favs', favs)
     console.log('hey were actually getting the user favs', productId)
-    const check = favs.indexOf(parseInt(productId))
+    const check = favs?.indexOf(parseInt(productId))
     console.log('what is the result of the check', check)
     const handleFavs = (id) => {
         console.log(id)
@@ -20,9 +20,13 @@ function FavButton({productId}){
     }
 
     return(
-        <button className='fav-button' onClick={() => handleFavs(productId)}>
-            {check === -1 ? <i class="fa-regular fa-heart"></i> : <i class="fa-solid fa-heart"></i>}
-        </button>
+        <>
+            {user && (
+                <button className='fav-button' onClick={() => handleFavs(productId)}>
+                {check === -1 ? <i class="fa-regular fa-heart"></i> : <i class="fa-solid fa-heart"></i>}
+                </button>
+            )}
+        </>
     )
 }
 
